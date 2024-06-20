67efc4a99040a32f0453e6b7f112f4e299eb838d
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-06-20 21:52:20 +0700

    order

--- app/src/main/java/com/example/myapplication/network/CartService.java
+++ app/src/main/java/com/example/myapplication/network/CartService.java
@@ -1,4 +1,5 @@
 package com.example.myapplication.network;
 
 public interface CartService {
+
 }
--- app/src/main/java/com/example/myapplication/network/OrderService.java
+++ app/src/main/java/com/example/myapplication/network/OrderService.java
@@ -1,4 +1,25 @@
 package com.example.myapplication.network;
 
+
+import com.example.myapplication.network.dto.request.OrderRequestDTO;
+import com.example.myapplication.network.dto.response.OrderDetailResponseDTO;
+import com.example.myapplication.network.dto.response.OrderResponseDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface OrderService {
+    @POST("api/order")
+    Call<Integer> saveOrder(@Body OrderRequestDTO orderRequestDTO);
+
+    @GET("api/order/customer/{customerId}")
+    Call<List<OrderResponseDTO>> getOrderByCustomerId(@Path("customerId") int customerId);
+
+    @GET("api/order/list")
+    Call<List<OrderResponseDTO>> getAllOrders();
 }
