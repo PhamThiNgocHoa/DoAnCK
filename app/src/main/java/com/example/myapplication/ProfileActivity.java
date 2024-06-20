151bec768db8892bae760907b2398aa658880d4b
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-06-20 16:03:35 +0700

    profile

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView titleUsername, profileName, profileEmail, profilePhone,profilePassword, profileAddress;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
         Button editButton = findViewById(R.id.editButton);
         ImageView back = findViewById(R.id.back);
+        titleUsername = findViewById(R.id.titleUsername);
+        profileName = findViewById(R.id.profileName);
+        profileEmail = findViewById(R.id.profileEmail);
+        profilePhone = findViewById(R.id.profilePhone);
+        profilePassword = findViewById(R.id.profilePassword);
+        profileAddress = findViewById(R.id.profileAddress);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
