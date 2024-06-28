a58f996c441f9284574407da846c02f5da960021
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2024-06-28 13:17:23 +0700

    done

--- app/src/main/java/com/example/myapplication/activity/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/activity/AdminActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -9,7 +11,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.example.myapplication.R;
 
 public class AdminActivity extends AppCompatActivity {
-    private ConstraintLayout thongKe, khachHang, sanPham, danhMuc, donHang, banHang;
+    private ConstraintLayout khachHang, sanPham, danhMuc, banHang;
     private ConstraintLayout thongKeBtn, donHangBtn;
 
     @SuppressLint("MissingInflatedId")
@@ -17,34 +19,34 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
-        thongKe = findViewById(R.id.thongKe);
-        khachHang = findViewById(R.id.khachHang);
-        sanPham = findViewById(R.id.sanPham);
-//        danhMuc = findViewById(R.id.danhMuc);
-        donHang = findViewById(R.id.donHang);
-//        banHang = findViewById(R.id.banHang);
-        khachHang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, CustomerListActivity.class);
-                startActivity(intent);
-            }
-        });
-        sanPham.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminActivity.this, ProductListActivity.class);
-                startActivity(intent);
-            }
         thongKeBtn = findViewById(R.id.thong_ke_btn);
         donHangBtn = findViewById(R.id.don_hang_btn);
-        thongKeBtn.setOnClickListener(v -> {
+        khachHang = findViewById(R.id.khachHang);
+//        sanPham = findViewById(R.id.sanPham);
+//        danhMuc = findViewById(R.id.danhMuc);
+
+//        banHang = findViewById(R.id.banHang);
+        khachHang.setOnClickListener(v -> {
+            Intent intent = new Intent(AdminActivity.this, CustomerListActivity.class);
+            startActivity(intent);
+        });
+        sanPham.setOnClickListener(v -> {
+            Intent intent = new Intent(AdminActivity.this, ProductListActivity.class);
+            startActivity(intent);
+        });
+        thongKeBtn = findViewById(R.id.thong_ke_btn);
+        donHangBtn = findViewById(R.id.don_hang_btn);
+        thongKeBtn.setOnClickListener(v ->
+        {
             Intent intent = new Intent(AdminActivity.this, StatisticalActivity.class);
             startActivity(intent);
         });
-        donHangBtn.setOnClickListener(v -> {
+        donHangBtn.setOnClickListener(v ->
+
+        {
             Intent intent = new Intent(AdminActivity.this, OrderListActivity.class);
             startActivity(intent);
         });
     }
 }
+
